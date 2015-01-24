4682a59d70b38f7cb08a48c04b703e1768fc1128
JoshHug <hug@cs.berkeley.edu>
2015-01-23 19:14:37 -0800

    added starter files for hw1

---
+++ hw1/TestCalcDistance.java
@@ -0,0 +1,42 @@
+
+/**
+ *  Tests calcDistance
+ */
+public class TestCalcDistance {
+
+    /**
+     *  Tests calcDistance.
+     */
+    public static void main(String[] args) {
+        checkCalcDistance();
+    }
+
+    /**
+     *  Checks whether or not two Doubles are equal and prints the result.
+     *
+     *  @param  d1      Double received
+     *  @param  d2      Expected double
+     *  @param  label   Label for the 'test' case
+     */
+    private static void checkEquals(double d1, double d2, String label) {
+        if (d1 == d2) {
+            System.out.println("PASS: " + label + ": " + d1 + " equal to " + d2);
+        } else {
+            System.out.println("FAIL: " + label + ": " + d1 + " not equal to " + d2);
+        }
+    }
+
+    /**
+     *  Checks the Planet class to make sure calcDistance works.
+     */
+    private static void checkCalcDistance() {
+        System.out.println("Checking calcDistance...");
+
+        Planet p1 = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+        Planet p2 = new Planet(2.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+        Planet p3 = new Planet(4.0, 5.0, 3.0, 4.0, 5.0, "jupiter.gif");
+
+        checkEquals(p1.calcDistance(p2), 1.0, "calcDistance()");
+        checkEquals(p1.calcDistance(p3), 5.0, "calcDistance()");
+    }
+}
---
+++ hw1/TestCalcPairwiseForce.java
@@ -0,0 +1,43 @@
+import java.math.*;
+
+/**
+ *  Tests calcPairwiseForce
+ */
+public class TestCalcPairwiseForce {
+
+    /**
+     *  Tests calcPairwiseForce.
+     */
+    public static void main(String[] args) {
+        checkCalcPairwiseForce();
+    }
+
+    /**
+     *  Checks whether or not two Doubles are equal and prints the result.
+     *
+     *  @param  d1      Double received
+     *  @param  d2      Expected double
+     *  @param  label   Label for the 'test' case
+     */
+    private static void checkEquals(double d1, double d2, String label) {
+        if (d1 == d2) {
+            System.out.println("PASS: " + label + ": " + d1 + " equal to " + d2);
+        } else {
+            System.out.println("FAIL: " + label + ": " + d1 + " not equal to " + d2);
+        }
+    }
+
+    /**
+     *  Checks the Planet class to make sure calcPairwiseForce works.
+     */
+    private static void checkCalcPairwiseForce() {
+        System.out.println("Checking calcPairwiseForce...");
+
+        Planet p1 = new Planet(1.0, 1.0, 3.0, 4.0, 5.0, "jupiter.gif");
+        Planet p2 = new Planet(2.0, 1.0, 3.0, 4.0, 4e11, "jupiter.gif");
+        Planet p3 = new Planet(4.0, 5.0, 3.0, 4.0, 5.0, "jupiter.gif");
+
+        checkEquals(p1.calcPairwiseForce(p2), 133.4, "calcPairwiseForce()");
+        checkEquals(p1.calcPairwiseForce(p3), 6.67e-11, "calcPairwiseForce()");
+    }
+}
