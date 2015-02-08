c9d7713cfe3e7785112e3893f8c371e0c9e296e3
jleath <lawnfurniture@icloud.com>
2015-02-08 04:39:41 -0500

    added tests for Board.java

---
+++ proj0/TestBoard.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** JUnit tests for the Board class.
+ *  @author Joshua Leath
+ */
+public class TestBoard {
+
+    /** 
+    
+    /** Run the tests for the Board class. */ 
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestBoard.class);
+    }
+}
