81f7e480dfcd99e89104c6bf89eece6556095482
JoshHug <hug@cs.berkeley.edu>
2015-02-27 19:01:07 -0800

    removed confusing sentence from get method

--- hw5/Map61B.java
+++ hw5/Map61B.java
@@ -11,17 +11,18 @@ public interface Map61B<K, V> {
     public boolean containsKey(K key);
 
     /* Returns the value to which the specified key is mapped, or null if this
-     * map contains no mapping for the key. You'll notice that the parameter
-     * type is K, not K. This odd choice follows Java conventions.
-     * Since all Ks have a .equals method, this won't really matter.
-     * Don't worry about this too much.
+     * map contains no mapping for the key. 
      */
     public V get(K key);
 
    /* Returns the number of key-value mappings in this map. */
     public int size();
 
-    /* Associates the specified value with the specified key in this map. */
+    /* Associates the specified value with the specified key in this map.
+     * Returns the previous value associated with key, or null if there was no
+     * mapping for key. (A null return can also indicate that the map previously
+     * associated null with key.)
+     */
     public void put(K key, V value);
 
     /* Removes the mapping for the specified key from this map if present.
@@ -30,7 +31,7 @@ public interface Map61B<K, V> {
 
     /* Removes the entry for the specified key only if it is currently mapped to
      * the specified value. Not required for HW5. */
-    public V remove(K key, V value);
+    public V remove(K key, K value);
 
     /* Returns a Set view of the keys contained in this map. Not required for HW5. */
     public Set<K> keySet();    
